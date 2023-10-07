aaa4f740abc14d5679127fb7a139023afcc6b9fc
Jack Cornelisse <jack.cornelisse@gmail.com>
2023-10-07 16:56:05 +0200

    adding cli tests

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -1,6 +1,29 @@
-from todo import hello_world
+from typer.testing import CliRunner
+from todo import app
 
-def test_hello_world(capsys):
-    hello_world('Gerald')
-    captured_prints = capsys.readouterr()
-    assert captured_prints.out == 'Hello Gerald!'
+
+runner = CliRunner()
+
+
+def test_hello_world():
+    result = runner.invoke(app, ['hello-world', 'Gerald'])
+    assert result.exit_code == 0
+    assert result.stdout.strip() == "Hello Gerald!"
+
+
+def test_goodbye_world():
+    result = runner.invoke(app, ['goodbye-world', 'Gerald'])
+    assert result.exit_code == 0
+    assert result.stdout.strip() == "Goodbye Gerald!"
+
+
+def test_add():
+    result = runner.invoke(app, ['add', '5', '3'])
+    assert result.exit_code == 0
+    assert result.stdout.strip() == '8'
+
+
+def test_add_default():
+    result = runner.invoke(app, ['add', '5'])
+    assert result.exit_code == 0
+    assert result.stdout.strip() == '6'
--- todo/todo.py
+++ todo/todo.py
@@ -1,7 +1,12 @@
-import typer
+from typer import Typer, Argument
 
 
-def hello_world(name: str):
+# Typer object that will keep all cli options and other details
+app = Typer(name="TODO", add_completion=False, no_args_is_help=True, help="CLI TODO is here to help your productivity!")
+
+
+@app.command()  # this app.command() will make the function available as cli
+def hello_world(name: str):  # underscores ('_') in function names will be replaced with dash ('-') in cli
     """Prints a welcome message.
 
     Args:
@@ -10,5 +15,27 @@ def hello_world(name: str):
     print(f'Hello {name}!')
 
 
+@app.command()
+def goodbye_world(name: str):
+    """Prints a goodbye message.
+
+    Args:
+        name (str): your name!
+    """
+    print(f'Goodbye {name}!')
+
+
+@app.command()
+def add(n1: int = Argument(..., help='An Integer'),  # typer has the elipse (...) as showing that it requires a value
+        n2: int = Argument(1, help='An Integer')):  # adding a value instead will allow for defaulting
+    """Adds two numbers.
+
+    Args:
+        n1 (int): first number.
+        n2 (int): second number. defaults to 1.
+    """
+    print(n1 + n2)
+
+
 if __name__ == "__main__":
-    typer.run(hello_world)
+    app()  # runs the typer object as cli
